c68b41e574951a1e4396b6a4914e23bed14be5ab
Alex Williams <alexsaniwilliams@gmail.com>
2016-08-08 14:53:21 -0400

    Added basic interfaces and Author/Post classes

---
+++ PostFeed/PostFeed/Domain/Author.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace PostFeed.Domain
+{
+    public class Author: IActivatable, IDbEntity
+    {
+        public int Id { get; set; }
+        public bool Active { get; set; }
+
+        string Name { get; set; }
+
+        ICollection<Post> Posts { get; set; }
+
+    }
+}
---
+++ PostFeed/PostFeed/Domain/Post.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PostFeed.Domain
+{
+    public class Post: IActivatable, IDbEntity
+    {
+        public int Id { get; set; }
+        public bool Active { get; set; }
+
+        public string Title { get; set; }
+        public string BodyText { get; set; }
+        public DateTime TimePosted { get; set; }
+        public Author PostCreator { get; set; }
+    }
+}
