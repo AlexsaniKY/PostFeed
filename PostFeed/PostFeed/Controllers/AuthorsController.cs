9693aee2b4c6ae5fc7df5846735f48fe1606f786
AlexsaniKY <cerafem@gmail.com>
2016-08-09 05:51:57 -0400

    Added WebApi controllers for Authors and Posts

---
+++ PostFeed/PostFeed/Controllers/AuthorsController.cs
@@ -0,0 +1,81 @@
+﻿using System.Linq;
+using System.Net;
+using System.Web.Http;
+using System.Web.Http.Description;
+using PostFeed.Domain;
+using PostFeed.Services;
+
+namespace PostFeed.Controllers
+{
+    public class AuthorsController : ApiController
+    {
+        private AuthorServices authorServices = new AuthorServices();
+
+        // GET: api/Authors
+        public IQueryable<Author> GetAuthors()
+        {
+            return authorServices.GetAll();
+        }
+
+        // GET: api/Authors/5
+        [ResponseType(typeof(Author))]
+        public IHttpActionResult GetAuthor(int id)
+        {
+            Author author = authorServices.Get(id);
+            if (author == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(author);
+        }
+
+        // PUT: api/Authors/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutAuthor(int id, Author author)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != author.Id)
+            {
+                return BadRequest();
+            }
+
+            authorServices.Update(author);
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Authors
+        [ResponseType(typeof(Author))]
+        public IHttpActionResult PostAuthor(Author author)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            authorServices.Add(author);
+
+            return CreatedAtRoute("DefaultApi", new { id = author.Id }, author);
+        }
+
+        // DELETE: api/Authors/5
+        [ResponseType(typeof(Author))]
+        public IHttpActionResult DeleteAuthor(int id)
+        {
+            Author author = authorServices.Get(id);
+            if (author == null)
+            {
+                return NotFound();
+            }
+
+            authorServices.Delete(author);
+
+            return Ok(author);
+        }
+    }
+}
---
+++ PostFeed/PostFeed/Controllers/PostsController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using PostFeed.Domain;
+using PostFeed.Infrastructure;
+using PostFeed.Services;
+
+namespace PostFeed.Controllers
+{
+    public class PostsController : ApiController
+    {
+        private PostServices postServices = new PostServices();
+        
+        // GET: api/Posts
+        public IQueryable<Post> GetPosts()
+        {
+            return postServices.GetAll();
+        }
+
+        // GET: api/Posts/5
+        [ResponseType(typeof(Post))]
+        public IHttpActionResult GetPost(int id)
+        {
+            Post post = postServices.Get(id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(post);
+        }
+
+        // PUT: api/Posts/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutPost(int id, Post post)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != post.Id)
+            {
+                return BadRequest();
+            }
+
+            postServices.Update(post);
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Posts
+        [ResponseType(typeof(Post))]
+        public IHttpActionResult PostPost(Post post)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            postServices.Add(post);
+
+            return CreatedAtRoute("DefaultApi", new { id = post.Id }, post);
+        }
+
+        // DELETE: api/Posts/5
+        [ResponseType(typeof(Post))]
+        public IHttpActionResult DeletePost(int id)
+        {
+            Post post = postServices.Get(id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+
+            postServices.Delete(id);
+
+            return Ok(post);
+        }
+    }
+}
