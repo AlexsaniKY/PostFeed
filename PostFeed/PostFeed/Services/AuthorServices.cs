0491e95dc99cee4864f5de6405cbd69f6ed9007e
AlexsaniKY <cerafem@gmail.com>
2016-08-09 04:44:11 -0400

    added service layer for all single class repositories

---
+++ PostFeed/PostFeed/Services/AuthorServices.cs
@@ -0,0 +1,17 @@
+﻿using PostFeed.Domain;
+using PostFeed.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PostFeed.Services
+{
+    public class AuthorServices: GenericServices<Author>
+    {
+        public AuthorServices()
+        {
+            _repo = new AuthorRepository(PostFeedDbContext.Create());
+        }
+    }
+}
---
+++ PostFeed/PostFeed/Services/PostServices.cs
@@ -0,0 +1,17 @@
+﻿using PostFeed.Domain;
+using PostFeed.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PostFeed.Services
+{
+    public class PostServices: GenericServices<Post>
+    {
+        public PostServices()
+        {
+            _repo = new PostRepository(PostFeedDbContext.Create());
+        }
+    }
+}
