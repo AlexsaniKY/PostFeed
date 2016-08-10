f096c86970b0cf8389dba0ffb30bea8b3b244255
Alex Williams <alexsaniwilliams@gmail.com>
2016-08-10 16:27:20 -0400

    preparing new user overlay form, not fully implemented

--- PostFeed/PostFeed/Scripts/NewUserForm.js
+++ PostFeed/PostFeed/Scripts/NewUserForm.js
@@ -1,1 +1,76 @@
-﻿
+﻿//http://jqueryui.com/dialog/#modal-form
+
+$(function () {
+    var dialog, form,
+        name = $("#name"),
+        allFields = $([]).add(name),
+        tips = $(".validateTips");
+    function updateTips(t) {
+        tips
+            .text(t)
+            .addClass("ui-state-highlight");
+        setTimeout(function () {
+            tips.removeClass("ui-state-highlight", 1500);
+        }, 500);
+    }
+
+    function checkLength(o, n, min, max) {
+        if (o.val().length > max || o.val().length < min) {
+            o.addClass("ui-state-error");
+            updateTips("Length of " + n + "must be between" +
+                min + " and " + max + ".");
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    function checkRegexp(o, regexp, n) {
+        if (!(regexp.test(o.val()))) {
+            o.addClass("ui-state-error");
+            updateTips(n);
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    function addUser() {
+        var valid = true;
+        allFields.removeClass("ui-state-error");
+
+        valid = valid && checkLength(name, "username", 3, 16);
+        valid = valid && checkRegexp(name, /^[a-z]([0-9a-z_\s])+$/i, "Username may consist of a-z, 0-9, underscores, spaces and must begin with a letter.");
+
+        if (valid) {
+            //use ajax to add the user
+        }
+        return valid;
+    }
+
+    dialog = $("#dialog-form").dialog({
+        autoOpen: false,
+        height: 400,
+        width: 350,
+        modal: true,
+        buttons: {
+            "Create a new User": addUser,
+            Cancel: function () {
+                dialog.dialog("close");
+            }
+        },
+        close: function () {
+            form[0].reset();
+            allFields.removeClass("ui-state-error");
+        }
+    });
+
+    form = dialog.find("form").on("submit", function (event) {
+        event.preventDefault();
+        addUser();
+    });
+
+    $("#newUser").button().on("click", function () {
+        dialog.dialog("open");
+    });
+});
