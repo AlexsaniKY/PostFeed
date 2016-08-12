1fd1708a38d47fd545698119ec8fb298a1ab01ae
AlexsaniKY <cerafem@gmail.com>
2016-08-12 16:54:46 -0400

    extended GetRecent and GetPosts(before/after) methods

--- PostFeed/PostFeed/Services/PostServices.cs
+++ PostFeed/PostFeed/Services/PostServices.cs
@@ -14,16 +14,39 @@ namespace PostFeed.Services
             _repo = new PostRepository(PostFeedDbContext.Create());
         }
 
+        public List<Post> GetRecent(int amount)
+        {
+            return GetAll()
+                .OrderByDescending(p => p.TimePosted)
+                .Take(amount)
+                .ToList();
+        }
+
         public List<Post> GetRecent(int amount, TimeSpan recency)
         {
             DateTime recentDate = DateTime.Now.Subtract(recency);
+            return GetPostsAfter(amount, recentDate);
+        }
+
+        public List<Post> GetPostsAfter(int amount, DateTime sinceDate)
+        {
             return GetAll()
                 //.GroupBy(p => p.TimePosted > recentDate)
-                .Where(p => p.TimePosted > recentDate)
+                .Where(p => p.TimePosted > sinceDate)
                 //.SelectMany(p => p)
                 .OrderByDescending(p => p.TimePosted)
                 .Take(amount)
                 .ToList();
+        }
+
+        public List<Post> GetPostsBefore(int amount, DateTime beforeDate)
+        {
+            return GetAll()
+                .Where(p => p.TimePosted < beforeDate)
+                .OrderByDescending(p => p.TimePosted)
+                .Take(amount)
+                .ToList();
+
         }
     }
 }
