3bd47ce147079a96121f386cf31bbaeb43487130
AlexsaniKY <cerafem@gmail.com>
2016-08-18 08:43:39 -0400

    IDbEntity documented

--- PostFeed/PostFeed/Domain/IDbEntity.cs
+++ PostFeed/PostFeed/Domain/IDbEntity.cs
@@ -6,6 +6,10 @@ using System.Threading.Tasks;
 
 namespace PostFeed.Domain
 {
+    /// <summary>
+    /// Allows an object to be stored in database with Key Id.
+    /// Requires int Id
+    /// </summary>
     public interface IDbEntity
     {
         int Id { get; set; }
