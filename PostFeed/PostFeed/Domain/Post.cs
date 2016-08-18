9770658bb7c3543cc652eeb6a515630531a6925e
AlexsaniKY <cerafem@gmail.com>
2016-08-18 08:52:27 -0400

    Post documented

--- PostFeed/PostFeed/Domain/Post.cs
+++ PostFeed/PostFeed/Domain/Post.cs
@@ -8,6 +8,9 @@ using System.Web;
 
 namespace PostFeed.Domain
 {
+    /// <summary>
+    /// Post is a message created by an Author on any desired subject
+    /// </summary>
     public class Post: IActivatable, IDbEntity
     {
         [Required]
@@ -15,17 +18,29 @@ namespace PostFeed.Domain
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public bool Active { get; set; }
-
+        /// <summary>
+        /// Title of the Post, to be displayed as a large header
+        /// </summary>
         public string Title { get; set; }
+        /// <summary>
+        /// Body of the Post, where most of the information should go
+        /// </summary>
         [Required]
         public string BodyText { get; set; }
+        /// <summary>
+        /// Time of the original Post.  Determined by server.
+        /// </summary>
         public DateTime TimePosted { get; set; }
-
+        /// <summary>
+        /// Id of the creator of this Post
+        /// </summary>
         [Required]
         public int AuthorId { get; set; }
         [ForeignKey("AuthorId")]
         public Author PostCreator { get; set; }
-
+        /// <summary>
+        /// This object owns no objects, no DeleteCascade necessary
+        /// </summary>
         public void DeleteCascade() { }
 
         public Post() { }
