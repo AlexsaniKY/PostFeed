aebbcbabe06bd9695c14d67ae6f379fbb93a116e
AlexsaniKY <cerafem@gmail.com>
2016-08-18 08:42:27 -0400

    Documented IActivatable

--- PostFeed/PostFeed/Domain/IActivatable.cs
+++ PostFeed/PostFeed/Domain/IActivatable.cs
@@ -6,6 +6,10 @@ using System.Threading.Tasks;
 
 namespace PostFeed.Domain
 {
+    /// <summary>
+    /// Allows an object to be "deleted" from a database without being removed.
+    /// Requires bool Active and DeleteCascade()
+    /// </summary>
     public interface IActivatable
     {
         bool Active { get; set; }
